3a5c7bb25924d09e8c3695805391e209132313d8
340144 <1352665867@qq.com>
2018-04-04 16:41:29 +0800

    新增上传文件

--- src/servlet/FileUpload.java
+++ src/servlet/FileUpload.java
@@ -1,29 +1,40 @@
 package servlet;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+@MultipartConfig      /////////////重要！！！！！(location = "D:\\")
 @WebServlet(name = "FileUpload",urlPatterns = "/FileUpload")
 public class FileUpload extends HttpServlet {
+
+    private static final String SAVE_PATH="D:\\lsy";//上传路径
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        PrintWriter outs=response.getWriter();
-        outs.print("成功");
-        response.setCharacterEncoding("GBK");
+        request.setCharacterEncoding("UTF-8");
         Part part=request.getPart("file");
-
         String header = part.getHeader("Content-Disposition");
         System.out.println(header);
-
-        String fileNames = header.substring(header.indexOf("filename=\"") + 10,
+        String fileNames = header.substring(header.indexOf("filename=\"") + 10,//获取文件名字
                 header.lastIndexOf("\""));
-        part.write(fileNames);
+
+
+        File f=new File(SAVE_PATH+File.separator);
+        if(!f.exists()){
+            f.mkdir();
+        }
+        part.write(SAVE_PATH+File.separator+fileNames);         //上传
+
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html");
+        PrintWriter outs=response.getWriter();
+        outs.print(fileNames+"  上传成功");
 
     }
 
